a2af16d30daadd3aff167e553dcbb3eeb05843f7
vime <vime@sofico.be>
2020-11-20 19:08:44 -0600

    Use html tags
    For this, we need to add the schema metadata property in the app
    module and set value to 'NO_ERRORS_SCHEMA' after the bootstrap property.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card.component';
@@ -13,6 +13,7 @@ import { CardComponent } from './card.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
